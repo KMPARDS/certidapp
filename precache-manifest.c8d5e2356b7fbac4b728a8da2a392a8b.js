72a487b6d2469e53af17abe0e715a5f06aae196f
zemse <sohamzemse143@gmail.com>
2020-01-22 14:57:48 +0530

    update fix

---
+++ precache-manifest.c8d5e2356b7fbac4b728a8da2a392a8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f356d5f4c098a0021f6511ceac35a4c3",
+    "url": "/certificate-react/index.html"
+  },
+  {
+    "revision": "1a962ed0f59a5ad1fe8c",
+    "url": "/certificate-react/static/css/main.7f4f9a84.chunk.css"
+  },
+  {
+    "revision": "000afca93b0db5650577",
+    "url": "/certificate-react/static/js/2.5ee00c6f.chunk.js"
+  },
+  {
+    "revision": "1a962ed0f59a5ad1fe8c",
+    "url": "/certificate-react/static/js/main.f241b0c9.chunk.js"
+  },
+  {
+    "revision": "03129387f157fe17ce43",
+    "url": "/certificate-react/static/js/runtime-main.e716f80e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/certificate-react/precache-manifest.86ce7c38528341e9fffdc6eff4f0393f.js"
+  "/certificate-react/precache-manifest.c8d5e2356b7fbac4b728a8da2a392a8b.js"
 );
 
 self.addEventListener('message', (event) => {
