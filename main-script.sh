ead1e77120bc1559cda25f0d46978858ba739a26
zemse <sohamzemse143@gmail.com>
2020-01-22 16:35:47 +0530

    update main script

--- main-script.sh
+++ main-script.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-npm run build && git checkout gh-pages && ./script.sh && git add . && git commit -m "build update $1" && git push && git checkout master
+npm run build && git checkout gh-pages && ./script.sh && git add . && git commit -m "build update $1" && git push
+git checkout master
