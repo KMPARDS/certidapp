726217a392c0d0d37b8bee0fbcc515e07b2a785c
zemse <sohamzemse143@gmail.com>
2020-01-22 17:08:01 +0530

    put hide button in navigation bar

--- src/containers/Navigation/Navigation.js
+++ src/containers/Navigation/Navigation.js
@@ -5,7 +5,9 @@ import { managerAddress } from '../../env';
 
 export default class extends Component {
   state = {
-    isManager: false
+    isManager: false,
+    showAllItems: true,
+    displayHideButton: false
   };
 
   componentDidMount = () => {
@@ -14,16 +16,36 @@ export default class extends Component {
       if(isManager !== this.state.isManager) {
         this.setState({ isManager });
       }
+
+      const navigationGroup = document.querySelector('.navigation-group');
+      if(navigationGroup) {
+        if(navigationGroup.offsetHeight > 56) {
+          this.setState({ displayHideButton: true });
+        } else {
+          this.setState({ displayHideButton: !this.state.showAllItems });
+        }
+      }
     }, 500);
   };
 
   render = () => (
     <div className="navigation-group">
-      <NavigationElement heading="Register Certificate" />
-      <NavigationElement heading="View Certificate" />
-      {this.state.isManager ? <NavigationElement heading="Add Certifier" /> : null}
-      <NavigationElement heading="List Authorities" />
-      <NavigationElement heading="Sign Certificate" />
+      {this.state.showAllItems ? <>
+        <NavigationElement heading="Register Certificate" />
+        <NavigationElement heading="View Certificate" />
+        {this.state.isManager ? <NavigationElement heading="Add Certifier" /> : null}
+        <NavigationElement heading="List Authorities" />
+        <NavigationElement heading="Sign Certificate" />
+      </> : null}
+      {this.state.displayHideButton ? <button
+        className="navigation-element"
+        style={{cursor: 'pointer'}}
+        onClick={() => this.setState({ showAllItems: !this.state.showAllItems })}
+      >
+        {this.state.showAllItems
+          ? <>Hide Menu Items</>
+          : <>Show Menu Items</>}
+      </button> : null}
     </div>
   );
 
