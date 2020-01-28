83b2df8034cd616fb19765afd26d26292a5e6e97
zemse <sohamzemse143@gmail.com>
2020-01-28 19:01:32 +0530

    work on sign certificate UI

--- src/containers/SignCertificate/SignCertificate.js
+++ src/containers/SignCertificate/SignCertificate.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Helmet } from "react-helmet";
 // import { certificateContract } from '../../env';
 import CSVReader from './CSVReader';
+import copy from 'copy-to-clipboard';
 
 const ethers = require('ethers');
 
@@ -16,10 +17,11 @@ export default class extends Component {
   state = {
     currentScreen: CURRENT_PAGE_ENUM.SELECT_MODE,
     name: '',
-    course: '',
+    subject: '',
     score: '',
-    extraData: '',
+    category: '',
     encodedCertificate: '',
+    copied: false,
     authorityName: '',
     isAuthorised: true,
     certificatesToSign: [],
@@ -63,37 +65,24 @@ export default class extends Component {
 
   signThisCertificate = async() => {
     try {
-      const nameBytes32 = window._z.stringToBytes32(this.state.name);
-      const qualificationBytes32 = window._z.encodeQualification(
-        this.state.course,
-        +this.state.score
-      );
-      const extraData = ethers.utils.hexlify(this.state.extraData || '0x');
-      if(extraData.length > 66) throw new Error('Extra Data Overflow, limit is 32 bytes');
-      const extraDataBytes32 = ethers.utils.hexZeroPad(extraData, 32);
-
-      const unsignedCertificateConcat = ethers.utils.hexlify(ethers.utils.concat([
-        nameBytes32,
-        qualificationBytes32,
-        extraDataBytes32
-      ]));
-
-      const unsignedCertificateHash = ethers.utils.keccak256(
-        ethers.utils.concat([ethers.utils.toUtf8Bytes('\x19Ethereum Signed Message:\n96'),unsignedCertificateConcat])
-      );
-
-      let signedCertificateConcat = unsignedCertificateConcat;
-
-      const signature = await window.signer.signMessage(ethers.utils.arrayify(unsignedCertificateConcat));
-      signedCertificateConcat = ethers.utils.concat([signedCertificateConcat, signature]);
-
-      this.setState({
-        encodedCertificate: ethers.utils.hexlify(signedCertificateConcat)
+      let encodedCertificate = window._z.encodeCertificateObject({
+        name: this.state.name,
+        subject: this.state.subject,
+        score: this.state.score,
+        category: this.state.category
       });
 
-      console.log('signed',ethers.utils.hexlify(signedCertificateConcat));
-    } catch (error) {
+      const signature = await window.signer.signMessage(ethers.utils.arrayify(encodedCertificate.dataRLP));
 
+      encodedCertificate = window._z.addSignaturesToCertificateRLP(encodedCertificate, signature);
+
+      this.setState({
+        certificateHex: encodedCertificate.fullRLP
+      });
+
+      // console.log('signed',ethers.utils.hexlify(signedCertificateConcat));
+    } catch (error) {
+      console.error(error.message);
     }
   };
 
@@ -189,24 +178,37 @@ export default class extends Component {
     } else if(this.state.currentScreen === CURRENT_PAGE_ENUM.SIGN_FRESH_CERTIFICATE) {
       screen = (
         <>
+        {this.state.certificateHex
+          ? <>
+          <p>Below is the signed certificate hex string. Send this hex string to certifiee and they can paste it in the 'Register Certificate' box on this ÐApp.</p>
+          <p style={{wordBreak:'break-all'}}>{this.state.certificateHex}</p>
+          <p style={{cursor: 'pointer'}} onClick={() => {
+            copy(this.state.certificateHex);
+            !this.state.copied && setTimeout(() => {
+              this.setState({ copied: false });
+            }, 1000);
+            this.setState({ copied: true });
+          }}>{this.state.copied ? '[ Copied! ]' : '[ Copy To Clipboard ]'}</p>
+          </>
+          : <>
         {header}
         <div className="form-group">
           <p>Enter Name:</p>
           <input
             className="certificate-textinput"
             type="text"
-            placeholder="Enter Certifiee Name (Max 30 chars)"
+            placeholder="Enter Certifiee Name"
             onChange={event => this.setState({name: event.target.value})}/>
         </div>
 
         <div className="form-group">
-          <p>Enter Course:</p>
+          <p>Enter Subject:</p>
           <input
             className="certificate-textinput"
             type="text"
-            placeholder="Enter Certifiee Course (Max 30 chars)"
+            placeholder="Enter Subject / Course Name"
             maxLength="30"
-            onChange={event => this.setState({course: event.target.value})}/>
+            onChange={event => this.setState({subject: event.target.value})}/>
         </div>
 
         <div className="form-group">
@@ -219,15 +221,16 @@ export default class extends Component {
         </div>
 
         <div className="form-group">
-          <p>Extra Data:</p>
+          <p>Certification Type / Category:</p>
           <input
             className="certificate-textinput"
             type="text"
-            placeholder="Enter Hex String 0x or can leave empty"
-            onChange={event => this.setState({extraData: event.target.value})}/>
+            placeholder="e.g. Participation / Merit / Appreciation"
+            onChange={event => this.setState({category: event.target.value})}/>
         </div>
 
         <button className="btn" onClick={this.signThisCertificate}>Sign this Certificate</button>
+        </>}
         </>
       );
     } else if(this.state.currentScreen === CURRENT_PAGE_ENUM.SIGN_ENCODED_CERTIFICATE) {
