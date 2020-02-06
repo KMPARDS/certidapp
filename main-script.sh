274f3d2d4f06970b4df6302c7056dbc5c3132a2b
zemse <sohamzemse143@gmail.com>
2020-02-06 18:23:18 +0530

    add stashing to gh pages deploy script

--- main-script.sh
+++ main-script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-npm run build && git checkout gh-pages && ./script.sh && git add . && git commit -m "build update $1" && git push
+npm run build && git stash && git checkout gh-pages && ./script.sh && git add . && git commit -m "build update $1" && git push
 git checkout master
+git stash pop
